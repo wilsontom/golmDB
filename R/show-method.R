f9003c8c6a187c1e72f871bf91d286fd6ff69147
wilsontom <wilsontom89@gmail.com>
2021-02-26 15:55:48 +0000

    S4 show methods

---
+++ R/show-method.R
@@ -0,0 +1,84 @@
+#' show-GolmAccession
+#' @rdname show
+#' @param a `GolmAccession` object
+#' @importFrom methods show
+#' @export
+
+setMethod('show', signature = 'GolmAccession',
+          function(object){
+
+
+            cat(cli::rule(
+              left = crayon::bold('GolmDB Accession Object'),
+              right = paste0('golmDB v', utils::packageVersion('golmDB'))
+            ), '\n')
+
+            cat(crayon::red(
+              'Object Size:',
+              format(utils::object.size(object), units = 'Kb'),
+              '\n',
+              '\n'
+            ))
+
+
+            cat(crayon:::green(
+              object@name, '\n'
+            ))
+
+            cat(crayon::green(
+              '# of peaks:', object@peaks
+            ))
+
+          }
+)
+
+
+
+#' show-GolmDB
+#' @rdname show
+#' @param a `GolmDB` object
+#' @importFrom methods show
+#' @export
+
+setMethod('show', signature = 'GolmDB',
+          function(object){
+
+
+            cat(cli::rule(
+              left = crayon::bold('GolmDB Database Object'),
+              right = paste0('golmDB v', utils::packageVersion('golmDB'))
+            ), '\n')
+
+            cat(crayon::red(
+              'Object Size:',
+              format(utils::object.size(object), units = 'Mb'),
+              '\n',
+              '\n'
+            ))
+
+            cat(crayon::green(
+              'Database Source:', object@source, '\n')
+            )
+
+            cat(crayon::green(
+              '# of Accessions:', length(object@accessions))
+            )
+
+
+
+
+
+
+
+          }
+)
+
+
+
+
+
+
+
+
+
+
